039b2e3f725b22eaaddb6120111b2277c31d391d
Vanessa Palacios <vanelunapalacios@live.com>
2017-02-20 18:39:13 -0500

    Add ngFor loop to show array of posts.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,14 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-  <h1>Devi Blog</h1>
-  <h2>{{month}} /{{day}}/{{year}}</h2>
-  <h3>{{ currentTagLine}}</h3>
+    <div class="container">
+      <h1>Devi Blog</h1>
+      <h5>{{month}}/{{day}}/{{year}}</h5>
+      <h3>{{currentTagLine}}</h3>
+      <h2>{{post.title}}</h2>
+      <ul>
+        <li *ngFor="let currentPost of posts">{{currentPost.title}}</li>
+      </ul>
   </div>
   `
 })
@@ -17,4 +21,12 @@ export class AppComponent{
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDay();
   year: number = this.currentTime.getFullYear();
+  posts: Post[]= [
+    new Post('How to Get Fit When You/re Really a Slob', 'All you really need to do is go for a walk, and by a walk I mean walk to your favorite gelatto place instead of driving there!', 'Fitness', 'Vanessa Sharma'),
+    new Post('Easy Recipes for the Uninspired Cook', 'Step one, get your phone. Step 2, call your favorite Thai place, Step 3, order!', 'Cooking', 'Vanessa Sharma')
+  ];
+}
+
+export class Post {
+  constructor(public title: string, public text: string, public category: string, public author: string){}
 }
