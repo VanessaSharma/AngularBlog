4b2abbe7ea9509ef32e52cc70ba963f14bb03e09
Vanessa Palacios <vanelunapalacios@live.com>
2017-02-20 15:44:21 -0500

    Add string and number variables to display current date and tagline.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,18 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Angular Blog</h1>
+  <div class="container">
+  <h1>Devi Blog</h1>
+  <h2>{{month}} /{{day}}/{{year}}</h2>
+  <h3>{{ currentTagLine}}</h3>
+  </div>
   `
 })
 
 export class AppComponent{
-
+  currentTagLine : string = 'For the real girl';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDay();
+  year: number = this.currentTime.getFullYear();
 }
