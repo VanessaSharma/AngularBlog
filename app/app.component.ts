b739c2ba1aa953d2736f74c1ddfe3ccda366523a
Vanessa Palacios <vanelunapalacios@live.com>
2017-02-20 19:14:54 -0500

    Add editPost functionality using event bindings.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,9 +9,9 @@ import {Component} from '@angular/core';
       <h3>{{currentTagLine}}</h3>
       <h2>{{post.title}}</h2>
       <ul>
-        <li *ngFor="let currentPost of posts">{{currentPost.title}}</li>
+        <li *ngFor="let currentPost of posts">{{currentPost.title}} <button (click)="editPost()">Edit</button></li>
       </ul>
-  </div>
+    </div>
   `
 })
 
@@ -22,10 +22,15 @@ export class AppComponent{
   day: number = this.currentTime.getDay();
   year: number = this.currentTime.getFullYear();
   posts: Post[]= [
-    new Post('How to Get Fit When You/re Really a Slob', 'All you really need to do is go for a walk, and by a walk I mean walk to your favorite gelatto place instead of driving there!', 'Fitness', 'Vanessa Sharma'),
+    new Post('How to Get Fit When You Are Really a Slob', 'All you really need to do is go for a walk, and by a walk I mean walk to your favorite gelatto place instead of driving there!', 'Fitness', 'Vanessa Sharma'),
     new Post('Easy Recipes for the Uninspired Cook', 'Step one, get your phone. Step 2, call your favorite Thai place, Step 3, order!', 'Cooking', 'Vanessa Sharma')
   ];
+
+  editPost() {
+    alert("You just requested to edit this post.");
+  }
 }
+
 
 export class Post {
   constructor(public title: string, public text: string, public category: string, public author: string){}
